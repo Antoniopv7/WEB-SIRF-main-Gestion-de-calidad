e0242e0ce6a75a128cfe3dc66d779966befd11d3
Antoniopv7 <antonio.perez@alumnos.ucentral.cl>
2024-05-26 23:09:56 -0400

    Por fin funciono esta mrd aleluya

--- src/models/ModelUser2.py
+++ src/models/ModelUser2.py
@@ -2,15 +2,21 @@ from .entities.Profesor import Profesor
 
 class ModelUser2():
     @classmethod
-    def login(self, db, profesor, cursor):
+    def login(self, db, profesor):
         try:
+            cursor = db.connection.cursor()
             sql = "SELECT id_profesor, nombre, password, rut FROM profesor WHERE rut = %s"
             cursor.execute(sql, (profesor.rut,))
             row = cursor.fetchone()
             if row:
-                profesor = Profesor(row[0], row[1], Profesor.check_password(row[2], profesor.password), row[3])
-                return profesor
+                if row[2] == profesor.password:  # Comparar directamente la contraseña
+                    profesor = Profesor(row[0], row[1], row[2], row[3])
+                    return profesor
+                else:
+                    return None
             else:
                 return None
         except Exception as ex:
             raise Exception(ex)
+        finally:
+            cursor.close()
